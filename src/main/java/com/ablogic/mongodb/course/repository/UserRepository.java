571191fa5a0e38f44a231402f9a3c5635ec89524
Adalberto Brasaca <aj-brasaca@hotmail.com>
2020-07-30 08:58:37 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ablogic/mongodb/course/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ablogic.mongodb.course.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ablogic.mongodb.course.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/ablogic/mongodb/course/services/UserService.java
@@ -0,0 +1,20 @@
+package com.ablogic.mongodb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ablogic.mongodb.course.domain.User;
+import com.ablogic.mongodb.course.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+}
