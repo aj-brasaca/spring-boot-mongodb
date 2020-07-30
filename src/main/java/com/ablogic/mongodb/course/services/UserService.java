3673c2c7ae0432151d172dbc1ff771c560f59dcd
Adalberto Brasaca <aj-brasaca@hotmail.com>
2020-07-30 15:20:17 -0300

    Obtendo um usuário por Id

--- src/main/java/com/ablogic/mongodb/course/services/UserService.java
+++ src/main/java/com/ablogic/mongodb/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ablogic.mongodb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ablogic.mongodb.course.domain.User;
 import com.ablogic.mongodb.course.repository.UserRepository;
+import com.ablogic.mongodb.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,12 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+	
+		Optional<User> user = userRepository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
