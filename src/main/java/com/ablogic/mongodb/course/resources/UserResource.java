cbf7546e652691b6d9a51bc4b83de06b43d81211
Adalberto Brasaca <aj-brasaca@hotmail.com>
2020-07-30 10:23:01 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/ablogic/mongodb/course/resources/UserResource.java
+++ src/main/java/com/ablogic/mongodb/course/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ablogic.mongodb.course.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ablogic.mongodb.course.domain.User;
+import com.ablogic.mongodb.course.dto.UserDTO;
 import com.ablogic.mongodb.course.services.UserService;
 
 @RestController
@@ -19,10 +21,11 @@ public class UserResource {
 	private UserService userService;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 				
 		List<User> list = userService.findAll();
-				
-		return ResponseEntity.ok().body(list);
+		// Convertendo a lista original "list" para a lista "listDTO"
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
